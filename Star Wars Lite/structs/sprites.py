fe1b9ceca8c6eb5acbf4899c3c033f3763b00ffe
Brian <brianbawden001@gmail.comgit config --global user.name Brianrungit config --global user.email brianbawden001@gmail.com>
2021-12-14 14:50:10 -0500

    added to the file paths to get the game to work

--- Star Wars Lite/structs/sprites.py
+++ Star Wars Lite/structs/sprites.py
@@ -4,20 +4,20 @@ WIDTH, HEIGHT = 750, 750
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # images of enemy ships
-ENEMY0 = pygame.image.load(r"assets\enemy1.png")
-ENEMY1 = pygame.image.load(r"assets\enemy2.png")
-ENEMY3 = pygame.image.load(r"assets\enemy3.png")
+ENEMY0 = pygame.image.load(r"Star Wars Lite/assets\enemy1.png")
+ENEMY1 = pygame.image.load(r"Star Wars Lite/assets\enemy2.png")
+ENEMY3 = pygame.image.load(r"Star Wars Lite/assets\enemy3.png")
 
 # image of player ship
-PLAYER = pygame.image.load(r"assets\promlf.png")
+PLAYER = pygame.image.load(r"Star Wars Lite\assets\promlf.png")
 
 # images of laser shots
-LASER_RED = pygame.image.load(r"assets\laser_red.png")
-LASER_GREEN = pygame.image.load(r"assets\laser_green.png")
-LASER_BLUE = pygame.image.load(r"assets\laser_blue.png")
-LASER_YELLOW = pygame.image.load(r"assets\laser_yellow.png")
+LASER_RED = pygame.image.load(r"Star Wars Lite\assets\laser_red.png")
+LASER_GREEN = pygame.image.load(r"Star Wars Lite\assets\laser_green.png")
+LASER_BLUE = pygame.image.load(r"Star Wars Lite\assets\laser_blue.png")
+LASER_YELLOW = pygame.image.load(r"Star Wars Lite\assets\laser_yellow.png")
 
 # image of background
 BACKGROUND = pygame.transform.scale(
-    pygame.image.load(r"assets\game_background.jpg"), (WIDTH, HEIGHT)
+    pygame.image.load(r"Star Wars Lite\assets\game_background.jpg"), (WIDTH, HEIGHT)
 )
